447cdab3ba941652392181eed219871c9458dbc4
Pavol Kirschbaum <pkirschbaum@sygic.com>
2016-01-29 11:04:31 +0100

    Require individual zf components only instead of whole zf

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,16 +6,12 @@ ini_set('error_reporting', E_ALL | E_STRICT);
 
 
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    $loader = require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/../vendor/autoload.php';
 } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
-    $loader = require __DIR__ . '/../../../autoload.php';
+    require __DIR__ . '/../../../autoload.php';
 } else {
     throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
-
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader->add('MonologModuleTest', __DIR__);
-unset($loader);
 
 
 if (file_exists(__DIR__ . '/configuration.php')) {
